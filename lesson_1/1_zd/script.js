6b4493f33840191277a078268fb9db0ac95028c2
Grek <deni.den.96@mail.ru>
2018-11-12 11:28:02 +0500

    Исправление ошибок кода 1 задание 1 урока

--- lesson_1/1_zd/script.js
+++ lesson_1/1_zd/script.js
@@ -1,16 +1,14 @@
-const loop = (t) => {
-	let times = t;
-	let callback = ()=>{
-		console.log("Hello Neo");
-	};
-	if (times != undefined) {
+const loop = (times, callback) => {
+	if (callback !== undefined) {
 		for (let i=0; i<times; i++){
 			callback();
 		}
 	} else {
 		times = 0;
 		callback = null;
-		console.log("Время не задано\n"+"callback = " + callback);
+		console.log("Время не задано\n"+"Или функция callback отсутствует");
 	}
 }
-loop();
+loop(21, ()=>{
+			console.log("Hello Neo");
+		});
