cd79237a5c5db1680814bb55515efc5c3a024aa3
Grek <deni.den.96@mail.ru>
2018-12-04 23:41:27 +0500

    удаление из главной ветки

--- lesson_7/app/src/components/PageLayout/Blog/LinkItem.jsx
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from 'react';
-
-const LinkItem = props => (
-	<li>
-		<a onClick={props.func} href={props.link}>{props.label}</a>
-	</li>
-);
-
-export default LinkItem;
